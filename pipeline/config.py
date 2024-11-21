47d40b8a01bae9778fa14e82c3e8a5722745f5c1
Babak Firoozi Fooladi <b.firoozi.f@gmail.com>
2024-11-21 20:54:41 +0200

    Configuration for column name change schema added

---
+++ pipeline/config.py
@@ -0,0 +1,11 @@
+VantaaOpenApplications_colnames = {
+    "id": "id",
+    "ammattiala": "field",
+    "tyotehtava": "job_title",
+    "tyoavain": "job_key",
+    "osoite": "address",
+    "haku_paattyy_pvm": "application_end_date",
+    "x": "longitude_wgs84",
+    "y": "latitude_wgs84",
+    "linkki": "link",
+}
