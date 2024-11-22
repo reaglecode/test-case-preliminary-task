90439fcae5c1b86913689f21c0e9355c7fab59ff
Firoozi Fooladi Babak <babak.firoozifooladi@aalto.fi>
2024-11-22 10:00:21 +0200

    Some annotations added. SQL insertion changed to pandas method.

--- pipeline/etl.py
+++ pipeline/etl.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import requests
-from sqlalchemy import create_engine, orm
-from datetime import date
-
+from sqlalchemy import create_engine
 from pydantic import TypeAdapter
 
 from .const import API_URL
@@ -31,12 +29,10 @@ class SimpleExtractor:
         
         response = self.fetch_data()
         response.raise_for_status()
-        adapter = TypeAdapter(OpenApplication)
 
         # validating the records data types:
         validated_items = [adapter.validate_python(x).model_dump() for x in response.json()]
         return pd.json_normalize(validated_items)
-
 
     def __call__(self) -> pd.DataFrame:
         return self.extract()
@@ -45,7 +41,6 @@ class SimpleExtractor:
 class SimpleTransformer:
     def __init__(self):
         self.rename_schema = VantaaOpenApplications_colnames
-
 
     def _rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
         # Rename based on the defined schema and drop irrelevant fields
@@ -59,17 +54,21 @@ class SimpleTransformer:
 
 
 class SimpleLoader:
-    def __init__(self, conn_str: str):
+    def __init__(self, conn_str: str) -> None:
         # Setup Engine
         # TODO: test the engine connection
         self.engine = create_engine(conn_str)
 
-    def load(self, df: pd.DataFrame) -> pd.DataFrame:
+    def load(self, df: pd.DataFrame) -> None:
         with self.engine.connect() as conn:
-            df.to_sql(VantaaOpenApplications.__name__, con = conn, if_exists="replace")
+            df.to_sql(
+                name=VantaaOpenApplications.__name__,
+                con=conn,
+                if_exists="replace"  # this argument needs to be adjusted accordingly
+                )
             conn.commit()
 
-    def __call__(self, df: pd.DataFrame) -> pd.DataFrame:
+    def __call__(self, df: pd.DataFrame) -> None:
         self.load(df=df)
 
 
